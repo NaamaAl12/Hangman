ccc3d70e8754c66eaf51797c1616e5d36aa6419d
NaamaAl12 <111929172+NaamaAl12@users.noreply.github.com>
2024-12-18 15:40:19 -0800

    HangmanMain.java

--- HangmanMain.java
+++ HangmanMain.java
@@ -1,12 +1,3 @@
-/* CS145
- * Class HangmanMain is the driver program for the Hangman program.  It reads a
- * dictionary of words to be used during the game and then plays a game with
- * the user.  This is a cheating version of hangman that delays picking a word
- * to keep its options open.  You can change the setting for SHOW_COUNT to see
- * how many options are still left on each turn.
- */
- 
- 
 import java.util.*;
 import java.io.*;
 
